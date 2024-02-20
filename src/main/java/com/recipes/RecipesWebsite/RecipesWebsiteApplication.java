23657ec6d662c3caf0732cccd74fef3e56cfcbd6
Mihnea Savin <mihnea.savin@gmail.com>
2024-02-20 18:20:39 +0200

    Initial commit

---
+++ src/main/java/com/recipes/RecipesWebsite/RecipesWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.recipes.RecipesWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesWebsiteApplication.class, args);
+	}
+
+}
