8ec0805bb1f0cda1fa69341574f9e3f16006adc4
Mihnea Savin <mihnea.savin@gmail.com>
2024-08-12 23:08:39 +0300

    Added clear icon to the search bar

--- client/src/app/components/SearchBar.js
+++ client/src/app/components/SearchBar.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import {useRef} from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import CircularProgress from '@mui/material/CircularProgress';
 import '../styles/SearchBar.css';
-import { useRef, useEffect } from 'react';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 function sleep(duration) {
@@ -17,13 +18,14 @@ function sleep(duration) {
 export default function SearchBar() {
     const [open, setOpen] = React.useState(false);
     const [options, setOptions] = React.useState([]);
+    const [searchValue, setSearchValue] = React.useState('');
     const loading = open && options.length === 0;
     const searchBarRef = useRef(null);
 
     React.useEffect(() => {
         // Ensure the buttonRef.current is available
         const current = searchBarRef.current;
-        if(current === null) {
+        if (current === null) {
             alert("SearchBarRef.current was null!");
             return undefined;
         }
@@ -35,7 +37,6 @@ export default function SearchBar() {
         firstChild.style.width = "90%";
         firstChild.children[0].style.display = "flex";
 
-        console.log("Ran Search Bar useEffect");
     }, []);
 
     React.useEffect(() => {
@@ -64,17 +65,21 @@ export default function SearchBar() {
         }
     }, [open]);
 
+    const handleClear = () => {
+        setSearchValue('');
+    };
+
     return (
         <Autocomplete
             id="asynchronous-demo"
             className="async-autocomplete"
             ref={searchBarRef}
-            sx={{width: '90%',
+            sx={{
+                width: '90%',
                 height: 38,
 
                 '& div .MuiInputBase-root': {
                     padding: "0 39px 0 0 !important"
-
                 }
             }}
             open={open}
@@ -84,6 +89,8 @@ export default function SearchBar() {
             onClose={() => {
                 setOpen(false);
             }}
+            freeSolo
+            clearOnBlur
             isOptionEqualToValue={(option, value) => option.title === value.title}
             getOptionLabel={(option) => option.title}
             options={options}
@@ -91,14 +98,13 @@ export default function SearchBar() {
             renderInput={(params) => (
                 <TextField
                     {...params}
-                    sx={{maxWidth:"100%", maxHeight:"100%", height: 50}}
+                    sx={{maxWidth: "100%", maxHeight: "100%", height: 50}}
                     InputProps={{
                         ...params.InputProps,
-                        endAdornment: (
-                            <React.Fragment>
-                                {loading ? <CircularProgress size={20} /> : null}
-                                {params.InputProps.endAdornment}
-                            </React.Fragment>
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon/>
+                            </InputAdornment>
                         ),
                     }}
                 />
@@ -108,19 +114,19 @@ export default function SearchBar() {
 }
 
 const topFilms = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: 'Pulp Fiction', year: 1994 },
+    {title: 'The Shawshank Redemption', year: 1994},
+    {title: 'The Godfather', year: 1972},
+    {title: 'The Godfather: Part II', year: 1974},
+    {title: 'The Dark Knight', year: 2008},
+    {title: '12 Angry Men', year: 1957},
+    {title: "Schindler's List", year: 1993},
+    {title: 'Pulp Fiction', year: 1994},
     {
         title: 'The Lord of the Rings: The Return of the King',
         year: 2003,
     },
-    { title: 'The Good, the Bad and the Ugly', year: 1966 },
-    { title: 'Fight Club', year: 1999 },
+    {title: 'The Good, the Bad and the Ugly', year: 1966},
+    {title: 'Fight Club', year: 1999},
     {
         title: 'The Lord of the Rings: The Fellowship of the Ring',
         year: 2001,
@@ -129,30 +135,30 @@ const topFilms = [
         title: 'Star Wars: Episode V - The Empire Strikes Back',
         year: 1980,
     },
-    { title: 'Forrest Gump', year: 1994 },
-    { title: 'Inception', year: 2010 },
+    {title: 'Forrest Gump', year: 1994},
+    {title: 'Inception', year: 2010},
     {
         title: 'The Lord of the Rings: The Two Towers',
         year: 2002,
     },
-    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-    { title: 'Goodfellas', year: 1990 },
-    { title: 'The Matrix', year: 1999 },
-    { title: 'Seven Samurai', year: 1954 },
+    {title: "One Flew Over the Cuckoo's Nest", year: 1975},
+    {title: 'Goodfellas', year: 1990},
+    {title: 'The Matrix', year: 1999},
+    {title: 'Seven Samurai', year: 1954},
     {
         title: 'Star Wars: Episode IV - A New Hope',
         year: 1977,
     },
-    { title: 'City of God', year: 2002 },
-    { title: 'Se7en', year: 1995 },
-    { title: 'The Silence of the Lambs', year: 1991 },
-    { title: "It's a Wonderful Life", year: 1946 },
-    { title: 'Life Is Beautiful', year: 1997 },
-    { title: 'The Usual Suspects', year: 1995 },
-    { title: 'Léon: The Professional', year: 1994 },
-    { title: 'Spirited Away', year: 2001 },
-    { title: 'Saving Private Ryan', year: 1998 },
-    { title: 'Once Upon a Time in the West', year: 1968 },
-    { title: 'American History X', year: 1998 },
-    { title: 'Interstellar', year: 2014 },
+    {title: 'City of God', year: 2002},
+    {title: 'Se7en', year: 1995},
+    {title: 'The Silence of the Lambs', year: 1991},
+    {title: "It's a Wonderful Life", year: 1946},
+    {title: 'Life Is Beautiful', year: 1997},
+    {title: 'The Usual Suspects', year: 1995},
+    {title: 'Léon: The Professional', year: 1994},
+    {title: 'Spirited Away', year: 2001},
+    {title: 'Saving Private Ryan', year: 1998},
+    {title: 'Once Upon a Time in the West', year: 1968},
+    {title: 'American History X', year: 1998},
+    {title: 'Interstellar', year: 2014},
 ];
