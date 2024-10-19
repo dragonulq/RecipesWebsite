19454475a92deddb8d6a2a9fcc2d0ea85cd5130f
Mihnea Savin <mihnea.savin@gmail.com>
2024-10-19 19:57:57 +0100

    pushing local changes to reset laptop

--- client/src/app/components/HeaderOption.js
+++ client/src/app/components/HeaderOption.js
@@ -1,16 +1,98 @@
 import * as React from 'react';
-import Popover from '@mui/material/Popover';
-import Typography from '@mui/material/Typography';
+import {useRef} from 'react';
 import Button from '@mui/material/Button';
 import '../styles/HeaderOption.css';
-import {forwardRef, useRef} from "react";
 import MyPopover from "@/src/app/components/MyPopover";
+import debounce from "lodash/debounce";
 
+export default function HeaderOption({optionName, optionsPopover}) {
+    const [mouseOnButton, setMouseOnButton] = React.useState(false);
+    const [mouseOnPopover, setMouseOnPopover] = React.useState(false);
+    const ref = useRef(null);
+    const popoverRef = useRef(null);
+    const mouseOnButtonRef = useRef(false);
+
+    let columns = optionsPopover[Object.keys(optionsPopover)[0]];
+    let columnsLen = columns.length;
+    let rowsLen= 0;
+    columns.forEach(col => rowsLen = Math.max(rowsLen, col[Object.keys(col)[0]].length));
+    rowsLen++; // account for titles row
+
+    // React.useEffect(() => {
+    //     if (!open) {
+    //         return undefined;
+    //     }
+    //     console.log("running effect");
+    //     setTimeout(() => {
+    //         const current = ref.current;
+    //         if (current === null) {
+    //             return undefined;
+    //         }
+    //
+    //         const popoverContainer = current.querySelector(".popover-container");
+    //         if (popoverContainer) {
+    //
+    //             popoverContainer.style.gridTemplateColumns = `repeat(${columnsLen}, 1fr)`;
+    //             popoverContainer.style.gridTemplateRows = `repeat(${rowsLen}, 1fr)`;
+    //             addItemsToGrid(optionsPopover, popoverContainer);
+    //         }
+    //     }, 0);
+    //
+    // }, [anchorEl]);
+
+
+    const handleClose = () => {
+        setAnchorEl(null);
+        setMouseOnButton(false);
+    };
+
+    const debounceClosePopover = debounce(() => {
+        console.log("debouncing");
+        setMouseOnButton(false);
+    }, 200);
+
+    const handleOnMouseEnter = (event) => {
+        popoverRef.current.exposedSetAnchorEl(event.currentTarget);
+    };
+
+    ///get sibling node in popover to be able to access it and just move anchor EL in Popover such that when
+    // I set anchorEL it doesnt trigger a button rerender but you can still set Anchor El properly
+
+// idk about above, but: move anchor el into popover and expose with useImperativeHandle callbacks that
+    // manipulate anchor el
+
+    const handleOnMouseLeave = () => {
+        popoverRef.current.exposedSetAnchorEl(null);
+    };
+
+    const handleOnMouseEnterContainer = () => {
+      console.log("233");
+    };
+
+
+    return (
+        <div className="header-option-container" onMouseEnter={handleOnMouseEnterContainer}>
+            <Button className="header-option-button"
+                    onMouseEnter={handleOnMouseEnter}
+                    onMouseLeave={handleOnMouseLeave}
+                    variant="text"
+                    disableRipple>
+                {optionName}
+            </Button>
+            <MyPopover handleClose={handleClose}
+                       optionsPopover={optionsPopover} ref={popoverRef}
+            />
+        </div>
+    );
+}
+
+/*This can be rewritten to generate a gridTemplateAreas property instead
+* Might do it later*/
 const addItemsToGrid = (optionsPopover, grid) => {
     const headerOptionName = Object.keys(optionsPopover)[0];
     const columns = optionsPopover[headerOptionName];
     let maxRowLen = 0;
-    for(let i = 0;i < columns.length;i++) {
+    for (let i = 0; i < columns.length; i++) {
         const gridItem = document.createElement('h4');
         gridItem.classList.add('grid-item');
         const columnTitle = Object.keys(columns[i])[0];
@@ -19,12 +101,12 @@ const addItemsToGrid = (optionsPopover, grid) => {
         grid.appendChild(gridItem);
     }
 
-    for(let j = 0;j < maxRowLen;j++) {
-        for(let i = 0;i < columns.length;i++) {
+    for (let j = 0; j < maxRowLen; j++) {
+        for (let i = 0; i < columns.length; i++) {
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
             const columnTitle = Object.keys(columns[i])[0];
-            if(columns[i][columnTitle].length >= j + 1) {
+            if (columns[i][columnTitle].length >= j + 1) {
                 gridItem.textContent = columns[i][columnTitle][j];
             }
             grid.appendChild(gridItem);
@@ -32,53 +114,3 @@ const addItemsToGrid = (optionsPopover, grid) => {
     }
 
 };
-
-export default function HeaderOption({ optionName, optionsPopover }) {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const ref = useRef(null);
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
-    let columns = optionsPopover[Object.keys(optionsPopover)[0]];
-    let columnsLen = columns.length;
-    let rowsLen = 0;
-    columns.forEach(col => rowsLen = Math.max(rowsLen, col[Object.keys(col)[0]].length));
-
-    React.useEffect(() => {
-        if(!open) {
-            return undefined;
-        }
-        setTimeout(() => {
-            const current = ref.current;
-            if(current === null) {
-                return undefined;
-            }
-
-            const popoverContainer = current.querySelector(".popover-container");
-            if(popoverContainer) {
-                popoverContainer.style.gridTemplateColumns = `repeat(${columnsLen}, 1fr)`;
-                popoverContainer.style.gridTemplateRows = `repeat(${rowsLen}, 1fr)`;
-                addItemsToGrid(optionsPopover, popoverContainer);
-            }
-        }, 0);
-
-    },[anchorEl]);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-
-    return (
-        <div>
-            <Button id="header-option-button"
-                aria-describedby={id} variant="text" onClick={handleClick} disableRipple>
-                {optionName}
-            </Button>
-            <MyPopover id={id} open={open} anchorEl={anchorEl} handleClose={handleClose} optionsPopover={optionsPopover} ref={ref} />
-        </div>
-    );
-}
--- client/src/app/components/MyPopover.js
+++ client/src/app/components/MyPopover.js
@@ -1,16 +1,34 @@
-import {forwardRef} from "react";
+import {forwardRef, useCallback, useImperativeHandle} from "react";
 import Popover from "@mui/material/Popover";
 import * as React from "react";
 
 
 const MyPopover = forwardRef((props, ref) => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+    const exposedSetAnchorEl = useCallback((value) => {
+        setAnchorEl(value);
 
+        console.log("Ran exposed setter");
+    }, []);
+
+    const getPopoverOpen = useCallback(() => {
+        return open;
+    }, [open]);
+
+    useImperativeHandle(ref, () => {
+       return {
+           exposedSetAnchorEl,
+           getPopoverOpen
+       };
+    });
     return (
         <Popover
-            id={props.id}
-            open={props.open}
-            anchorEl={props.anchorEl}
-            onClose={props.handleClose}
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={() => {}}
             ref={ref}
             anchorOrigin={{
                 vertical: 'bottom',
